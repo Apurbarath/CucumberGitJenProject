43597203aa3c4fe57e258f8eb11e7174da051e81
Apurbarath <apurbarath04@gmail.com>
2021-03-01 18:52:08 +0530

    added cucumber plugins

--- src/test/java/runner/MyRunner.java
+++ src/test/java/runner/MyRunner.java
@@ -8,6 +8,10 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(features="feature/Simple.feature",
+				plugin = {"pretty","html:target/cucumber-html-report",
+						"json:target/cucumber.json","junit:target/cucumber.xml",
+						"return:target/return.txt"
+				},
 				glue = {"stepDefinations"},
 				dryRun=false ,
 				monochrome=true,
